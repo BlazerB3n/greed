0ba409210739388058e8f5ecc793717965f555fe
BlazerB3n <pit20003@byui.edu>
2022-02-22 11:51:24 -0700

    cast

--- game/Casting/cast.cs
+++ game/Casting/cast.cs
@@ -11,28 +11,59 @@ namespace Greed.Game.Casting
 
         }
 
-        public void AddActor()
+        public void AddActor(string group, Actor actor)
         {
+            if (!actors.ContainsKey(group))
+            {
+                actors[group] = new List<Actor>();
+            }
+
+            if (!actors[group].Contains(actor))
+            {
+                actors[group].Add(actor);
+            }
 
         }
 
-        public List<Actor> GetActors()
+        public List<Actor> GetActors(string groups)
         {
-            return null;
+            List<Actor> results = new List<Actor>();
+            if (actors.ContainsKey(groups))
+            {
+                results.AddRange(actors[groups]);
+            }
+            return results;
         }
 
         public List<Actor> GetAllActors()
-        {
-            return null;
+        {   
+            List<Actor> results = new List<Actor>();
+            foreach (List<Actor> result in actors.Values)
+            {
+                results.AddRange(result);
+            }
+            return results;
         }
 
-        public Actor GetFirstActor()
+        public Actor GetFirstActor(string groups)
         {
-            return null;
+            Actor result = null;
+            if (actors.ContainsKey(groups))
+            {
+                if (actors[groups].Count > 0)
+                {
+                    result = actors[groups][0];
+                }
+            }
+            return result;
         }
 
-        public void RemoveActor()
+        public void RemoveActor(string groups, Actor actor)
         {
+            if (actors.ContainsKey(groups))
+            {
+                actors[groups].Remove(actor);
+            }
 
         }
 
