180df764017affc77b5a83908b664562fb44dbe7
BlazerB3n <pit20003@byui.edu>
2022-02-21 17:53:34 -0700

    button

--- game/Casting/actor.cs
+++ game/Casting/actor.cs
@@ -48,7 +48,7 @@ namespace Greed.Game.Casting
         /// Gets the Actor's Volocity.
         /// </summary>
         /// <returns>The Volocity.</returns>
-        public Vector2 GetVolocity()
+        public Vector2 GetVelocity()
         {
             return velocity;
         }
--- game/Services/TextureService.cs
+++ game/Services/TextureService.cs
@@ -14,12 +14,12 @@ namespace Greed.Game.Services
         public void SetupTextures()
         {
             TextureRegistry.PLAYER = Raylib.LoadTexture(TextureRegistry.TEXTURE_PATH_Battler);
-            TextureRegistry.BOTTON = Raylib.LoadTexture(TextureRegistry.TEXTURE_PATH_BUTTONS);
+            TextureRegistry.BUTTON = Raylib.LoadTexture(TextureRegistry.TEXTURE_PATH_BUTTONS);
             TextureRegistry.ICONS = Raylib.LoadTexture(TextureRegistry.TEXTURE_PATH_icons);
 
             // Raylib.LoadTextureCubemap
 
-            TextureRegistry.BOTTON_TextureID = (int) TextureRegistry.BOTTON.id;
+            TextureRegistry.BUTTON_TextureID = (int) TextureRegistry.BUTTON.id;
             TextureRegistry.PLAYER_TextureID = (int) TextureRegistry.PLAYER.id;
             TextureRegistry.ICONS_TextureID = (int) TextureRegistry.ICONS.id;
         }
