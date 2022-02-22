9e2bec15779cf496426f366b1abd1666cb158bd4
BlazerB3n <pit20003@byui.edu>
2022-02-22 16:11:11 -0700

    inputservice/point

--- game/Services/InputService.cs
+++ game/Services/InputService.cs
@@ -1,12 +1,52 @@
 using Raylib_cs;
+using System.Numerics;
+using Greed.Game.Casting;
 
 namespace Greed.Game.Services
 {
     class InputService
     {
-        public InputService()
-        {
+        
 
+        public InputService(int CELL_SIZE)
+        {
+            SYSTEM_SETTINGS.CELL_SIZE = CELL_SIZE;
+
+        }       
+
+        public Point GetDirection()
+        {
+            int dx = 0;
+            int dy = 0;
+            
+
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+            {
+                dx = -1;
+            }
+
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+            {
+                dx = 1;
+            }
+
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+            {
+                dy = -1;
+            }
+
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+            {
+                dy = 1;
+            }
+            
+
+        
+            Point direction = new Point(dx, dy);
+            
+            direction = direction.Scale(SYSTEM_SETTINGS.CELL_SIZE);
+
+            return direction;
         }
 
         
